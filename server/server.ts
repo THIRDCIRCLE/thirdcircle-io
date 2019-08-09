7a88bde238876ac6e506d9ac53c1d35793b52661
Peter Andreas Moelgaard <gitkraken@petermolgaard.com>
2019-08-09 22:26:01 +0200

    first functional walking skeleton

---
+++ server/server.ts
@@ -0,0 +1,38 @@
+import * as express from "express";
+import {Server as SocketServer} from "socket.io";
+import * as path from "path";
+
+const app = express();
+app.set("port", process.env.PORT || 3000);
+
+app.set('views', './client');
+app.set('view engine', 'pug');
+
+let http = require("http").Server(app);
+
+let io: SocketServer = require("socket.io")(http);
+
+// simple '/' endpoint sending a Hello World
+// response
+app.get("/", (req: any, res: any) => {
+    res.render('index');
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+io.on("connection", function (socket: any) {
+
+    console.log("a user connected");
+
+    socket.on("message", function (message: any) {
+
+        console.log(message);
+
+        socket.emit("message", message);
+    });
+});
+
+// start our simple server up on localhost:3000
+const server = http.listen(3000, function () {
+    console.log("listening on *:3000");
+});
