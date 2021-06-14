0d554fda768800b252fe9c20b084f2d0d30d3e61
Peter Andreas Moelgaard <gitkraken@petermolgaard.com>
2021-06-14 17:34:32 +0700

    Configuration Update

--- src/configurations/socket-io.ts
+++ src/configurations/socket-io.ts
@@ -4,8 +4,7 @@ import HttpServer = TC.IO.HttpServer;
 module.exports = {
   socketIo: {
     [HttpServer.Configuration.Keys.CORS]: {
-      origin: ['https://localhost:4200'],
-      credentials: true
+      origin: '*',
     }
   },
 };
